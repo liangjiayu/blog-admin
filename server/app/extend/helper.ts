2a9d5c14ddf69df17646ad2452fb1e2151e702c4
LiangJiaYu <1013834609@qq.com>
2021-01-09 15:08:19 +0800

    fix: 辅助工具

--- server/app/extend/helper.ts
+++ server/app/extend/helper.ts
@@ -1,14 +1,6 @@
 import { IHelper } from 'egg';
 import Schema from 'async-validator';
 
-interface pageOption {
-  tableName:string;
-  sql: string;
-  value: any[];
-  pageSize: number;
-  pageNum: number;
-}
-
 export default {
   /**
    * 处理成功，统一返回给客户端数据结构
@@ -68,75 +60,5 @@ export default {
         this.ctx.throw(422, 'Validate Error', { errors, fields });
       }
     });
-  },
-
-  /**
-   * 查询mysql是否插入成功
-   * @param this helper
-   * @param info mysql处理信息
-   */
-  checkMysqlInsert(this: IHelper, info) {
-    if (info.affectedRows === 1) {
-      return true;
-    }
-    return false;
-  },
-
-  /**
-   * 查询mysql是否删除成功
-   * @param this helper
-   * @param info mysql处理信息
-   */
-  checkMysqlDelate(this: IHelper, info) {
-    if (info.affectedRows === 1) {
-      return true;
-    }
-    throw new Error('Mysql删除失败');
-  },
-
-  /**
-   * 查询mysql是否更新成功
-   * @param this helper
-   * @param info mysql处理信息
-   */
-  checkMysqlUpdate(this: IHelper, info) {
-    if (info.affectedRows === 1) {
-      return true;
-    }
-    throw new Error('Mysql更新失败');
-  },
-
-  /**
-   * 列表分页
-   * @param this IHelper
-   * @param option sql查询
-   */
-  async baseListPage(this: IHelper, option: pageOption) {
-    const _option = {
-      tableName: option.tableName || '',
-      sql: option.sql || '',
-      value: option.value || [],
-      pageNum: Number(option.pageNum) || 1,
-      pageSize: Number(option.pageSize) || 10,
-    };
-
-    const _sql = `${_option.sql} LIMIT ?,?`;
-    const _sqlValue = [
-      ..._option.value,
-      (_option.pageNum - 1) * _option.pageSize,
-      _option.pageSize,
-    ];
-
-    const result = {
-      row: [],
-      total: 0,
-      current: _option.pageNum,
-      size: _option.pageSize,
-    };
-
-    result.row = await this.app.mysql.query(_sql, _sqlValue);
-    result.total = await this.app.mysql.count(_option.tableName);
-
-    return result;
   },
 };
