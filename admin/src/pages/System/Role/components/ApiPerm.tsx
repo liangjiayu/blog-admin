6cb3882a1a873597f6b8ecd57c05453fa7a32c2e
LiangJiaYu <1013834609@qq.com>
2021-02-21 16:20:39 +0800

    featrue: API权限

--- admin/src/pages/System/Role/components/ApiPerm.tsx
+++ admin/src/pages/System/Role/components/ApiPerm.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Modal, Tree, message } from 'antd';
 
 import { updateRole } from '@/api/role';
+import ApiAuth from '../ApiAuth.json';
 
 type ApiPermProps = {
   onCancel: () => void;
@@ -10,61 +11,19 @@ type ApiPermProps = {
   current: any;
 };
 
-const apiGroup = [
-  { name: '用户管理', value: 'user' },
-  { name: '角色管理', value: 'role' },
-];
-
-const apiData = [
-  {
-    name: '新增用户',
-    value: 'user:add',
-    apiGroup: 'user',
-  },
-  {
-    name: '删除用户',
-    value: 'user:del',
-    apiGroup: 'user',
-  },
-  {
-    name: '新增角色',
-    value: 'role:add',
-    apiGroup: 'role',
-  },
-  {
-    name: '删除角色',
-    value: 'role:del',
-    apiGroup: 'role',
-  },
-];
-
-const handleApiTree = () => {
-  const tree: any[] = [];
-  apiGroup.forEach((item) => {
-    const row: any = {};
-
-    const rowChildren: any[] = [];
-
-    apiData.forEach((sub) => {
-      if (sub.apiGroup === item.value) {
-        rowChildren.push({
-          title: sub.name,
-          key: sub.value,
-        });
-      }
-    });
-
-    row.title = item.name;
-    row.key = item.value;
-    row.children = rowChildren;
-
-    tree.push(row);
+const handleApiTree = (data) => {
+  const tree = data.map((item) => {
+    return {
+      title: item.name,
+      key: item.value,
+      children: item.children ? handleApiTree(item.children) : [],
+    };
   });
 
   return tree;
 };
 
-const apiTree = handleApiTree();
+const apiTree = handleApiTree(ApiAuth);
 
 const ApiPerm: React.FC<ApiPermProps> = (props) => {
   const [checkedKeys, setCheckedKeys] = useState([]);
@@ -74,21 +33,7 @@ const ApiPerm: React.FC<ApiPermProps> = (props) => {
   };
 
   const onSubmit = () => {
-    // 过滤分组
-    const apiPerm = checkedKeys.filter((item) => {
-      const flag = apiGroup.some((sub) => {
-        if (sub.value === item) {
-          return true;
-        }
-        return false;
-      });
-      if (flag) {
-        return false;
-      }
-      return true;
-    });
-
-    updateRole({ roleId: props.current.roleId, apiPerm: apiPerm.join() }).then(() => {
+    updateRole({ roleId: props.current.roleId, apiPerm: checkedKeys.join() }).then(() => {
       message.success('提交成功');
       props.onSuccess();
     });
